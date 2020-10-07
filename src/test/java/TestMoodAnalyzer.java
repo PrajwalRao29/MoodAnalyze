7321cb4a03d982514eb13983423d354de2306e58
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-07 20:43:18 +0530

    Testing to return happy when string sad isnt found

--- src/test/java/TestMoodAnalyzer.java
+++ src/test/java/TestMoodAnalyzer.java
@@ -4,7 +4,11 @@ import org.junit.Test;
 public class TestMoodAnalyzer {
 MoodAnalyzer m=new MoodAnalyzer();
 @Test
-        public void testAnalyzeMood() {
+        public void test1AnalyzeMoodWhenSad() {
     Assert.assertEquals(m.analyzeMood("I am in sad mood"), "Sad");
 }
+    @Test
+    public void test2AnalyzeMoodWhenNotSad() {
+        Assert.assertEquals(m.analyzeMood("I am in Any mood"), "Happy");
+    }
 }

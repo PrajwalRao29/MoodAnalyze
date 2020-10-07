2dbdb4b39ca928536494fa37d5117b4599fd5a8c
PrajwalRao29 <raoprajrao@gmail.com>
2020-10-07 22:08:08 +0530

    Added functionality to throw exception if mood is null

--- src/test/java/TestMoodAnalyzer.java
+++ src/test/java/TestMoodAnalyzer.java
@@ -2,20 +2,29 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMoodAnalyzer {
-MoodAnalyzer m;
-@Test
-        public void test1AnalyzeMoodWhenSad() {
-    m=new MoodAnalyzer("I am in sad mood");
-    Assert.assertEquals(m.analyzeMood(), "Sad");
-}
+    MoodAnalyzer m;
+
     @Test
-    public void test2AnalyzeMoodWhenNotSad() {
-        m=new MoodAnalyzer("I am in Any mood");
+    public void test1AnalyzeMoodWhenSad() {
+        m = new MoodAnalyzer("I am in sad mood");
+        Assert.assertEquals(m.analyzeMood(), "Sad");
+    }
+    @Test
+    public void test2AnalyzeMoodWhenHappy() {
+        m = new MoodAnalyzer("I am in Happy mood");
         Assert.assertEquals(m.analyzeMood(), "Happy");
     }
+
+    @Test
+    public void test3AnalyzeMoodWhenEmpty() {
+        m = new MoodAnalyzer("I am in Any mood");
+        Assert.assertEquals(m.analyzeMood(), "Invalid Mood");
+    }
+
     @Test
     public void test2AnalyzeMoodWhenNull() {
-        m=new MoodAnalyzer();
-        Assert.assertEquals(m.analyzeMood(), "Happy");
+        m = new MoodAnalyzer();
+        Assert.assertEquals(m.analyzeMood(), "Invalid Mood");
     }
 }
+
